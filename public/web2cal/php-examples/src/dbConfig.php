7f1cc579ca2df3c4e26f72b170b98decd96708be
taka <taka@fullmarks.co.jp>
2016-09-29 15:39:47 +0900

    不要ファイル削除

--- public/web2cal/php-examples/src/dbConfig.php
+++
@@ -1,7 +0,0 @@
-<?
-	define("EZSQL_DB_USER", "root");			// <-- mysql db user
-	define("EZSQL_DB_PASSWORD", "password");		// <-- mysql db password
-	define("EZSQL_DB_NAME", "web2caldemo");		// <-- mysql db pname
-	define("EZSQL_DB_HOST", "localhost");	// <-- mysql server host
-
-	?>
